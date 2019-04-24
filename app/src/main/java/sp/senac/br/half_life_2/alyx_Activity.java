29d894c854371eb495f80cf616a495074ed13ebc
gustavo.sferreira3 <gustavogferreira00@gmail.com>
2019-04-24 12:18:05 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/half_life_2/alyx_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.half_life_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alyx_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alyx_layout);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/half_life_2/gman_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.half_life_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gman_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gman_layout);
+    }
+}
